619539bd1b2ecc43ff787ffedbfca1767e66f386
Bary Levi <barylevi@gmail.com>
2021-04-12 23:36:32 +0300

    add support for media files

--- yanki/models.py
+++ yanki/models.py
@@ -1,6 +1,33 @@
 import genanki
+from typing import Optional
 
-main_model = genanki.Model(
+
+class ModelWithLatexPreamble(genanki.Model):
+    def __init__(
+            self,
+            latex_pre: Optional[str] = None,
+            latex_post: Optional[str] = None,
+            latex_svg : bool = False,
+            *args,
+            **kwargs):
+        super().__init__(*args, **kwargs)
+        self.latex_pre = latex_pre
+        self.latex_post = latex_post
+        self.latex_svg = latex_svg
+
+    def to_json(self, *args, **kwargs):
+        json = super().to_json(*args, **kwargs)
+        if self.latex_pre is not None:
+            json['latexPre'] = self.latex_pre
+        if self.latex_post is not None:
+            json['latexPost'] = self.latex_post
+
+        json['latexsvg'] = self.latex_svg
+
+        return json
+        
+
+main_model = ModelWithLatexPreamble(
     model_id=559856851,
     fields=[
         {
@@ -24,5 +51,28 @@ main_model = genanki.Model(
     text-align: center;
     color: black;
     background-color: white;
-}'''
+}''',
+    latex_pre=r'''
+\documentclass[border=1px]{standalone}
+
+\usepackage[utf8]{inputenc}
+
+\usepackage{siunitx}
+\sisetup{
+  per-mode=fraction,
+  inter-unit-product = \ensuremath{{}\cdot{}}
+}
+\DeclareSIUnit\atm{atm}
+\DeclareSIUnit\psi{psi}
+
+\usepackage{chemmacros}
+\usepackage{chemfig}
+\chemsetup{modules = newman}
+
+\pagestyle{empty}
+\setlength{\parindent}{0in}
+\begin{document}
+    ''',
+    name='yanki_default_model',
+    latex_svg=True
 )
--- yanki/package.py
+++ yanki/package.py
@@ -1,18 +1,29 @@
 import genanki
+import itertools
 from dataclasses import dataclass
-from typing import List
+from typing import List, Iterable
 from pathlib import Path
 from .decks import YamlDeck, DECK_YAML
+
+MEDIA_GLOBS = [
+    '**/*.jpg', '**/*.png', '**/*.gif', '**/*.tiff', '**/*.svg', '**/*.tif', '**/*.jpeg', '**/*.mp3',
+    '**/*.ogg', '**/*.wav', '**/*.avi', '**/*.ogv', '**/*.mpg', '**/*.mpeg', '**/*.mov', '**/*.mp4',
+    '**/*.mkv', '**/*.ogx', '**/*.ogv', '**/*.oga', '**/*.flv', '**/*.swf', '**/*.flac', '**/*.webp',
+    '**/*.m4a'
+]
 
 
 @dataclass
 class Package:
     decks: List[YamlDeck]
+    media: List[Path]
 
     @staticmethod
     def from_dir(base_dir: Path):
         deck_files = base_dir.glob('**/' + DECK_YAML)
         decks: List[YamlDeck] = []
+
+        print('collecting subdesk...')
 
         deck_file: Path
         for deck_file in deck_files:
@@ -23,7 +34,14 @@ class Package:
                 .replace('/', '::')
             decks.append(YamlDeck.from_dir(name=name, dir=deck_dir))
 
-        return Package(decks=decks)
+        print(f'Found subdecks: {len(decks)}')
+        print('Collecting media...')
+        media = list(itertools.chain.from_iterable(
+            base_dir.glob(media_pattern) for media_pattern in MEDIA_GLOBS))
+        print(f'Found media files: {len(media)}')
+        return Package(decks=decks, media=media)
 
     def to_genanki_package(self):
-        return genanki.Package([deck.to_genanki_deck() for deck in self.decks])
+        return genanki.Package(
+            [deck.to_genanki_deck() for deck in self.decks],
+            media_files=self.media)
